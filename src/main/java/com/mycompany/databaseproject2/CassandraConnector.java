c89132304e07473d57398dc5dca42ba409c0ee2e
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 18:54:33 +0200

    Removed obsolete comments

--- src/main/java/com/mycompany/databaseproject2/CassandraConnector.java
+++ src/main/java/com/mycompany/databaseproject2/CassandraConnector.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.databaseproject2;
 
 import com.datastax.driver.core.Cluster;
@@ -10,11 +5,6 @@ import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Session;
 
-/**
- * 
- * This is an implementation of a simple Java client.
- *
- */
 public class CassandraConnector {
 
     private Cluster cluster;
