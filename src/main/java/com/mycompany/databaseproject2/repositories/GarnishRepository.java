52d882913bf7a3ae03af77526a3a6894dda4f648
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-19 00:40:18 +0200

    Finished writing the garnish repository

--- src/main/java/com/mycompany/databaseproject2/repositories/GarnishRepository.java
+++ src/main/java/com/mycompany/databaseproject2/repositories/GarnishRepository.java
@@ -6,6 +6,9 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author KasperNissen1997
@@ -33,5 +36,54 @@ public class GarnishRepository {
         session.execute(query);
     }
     
+    /**
+     * Insert a row in the table garnish. 
+     * 
+     * @param garnish
+     */
+    public void insertGarnish(Garnish garnish) {
+        StringBuilder sb = new StringBuilder("INSERT INTO ")
+                .append(TABLE_NAME)
+                .append("(name) ")
+                .append("VALUES (\'")
+                .append(garnish.getName())
+                .append("\');");
+
+        final String query = sb.toString();
+        session.execute(query);
+    }
     
+    /**
+     * Select all garnish' from garnish
+     * 
+     * @return
+     */
+    public List<Garnish> selectAll() {
+        StringBuilder sb = new StringBuilder("SELECT * FROM ")
+                .append(TABLE_NAME);
+
+        final String query = sb.toString();
+        ResultSet rs = session.execute(query);
+
+        List<Garnish> garnishes = new ArrayList<Garnish>();
+
+        for (Row r : rs) {
+            Garnish garnish = new Garnish(r.getString("name"));
+            garnishes.add(garnish);
+        }
+        return garnishes;
+    }
+    
+    /**
+     * Delete table.
+     * 
+     * @param tableName the name of the table to delete.
+     */
+    public void deleteTable(String tableName) {
+        StringBuilder sb = new StringBuilder("DROP TABLE IF EXISTS ")
+                .append(tableName);
+
+        final String query = sb.toString();
+        session.execute(query);
+    }
 }
