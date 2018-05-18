be10e44f8a3508697efde59070af4f45821b9eea
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 23:36:27 +0200

    Started implementing some form of functionality

--- src/main/java/com/mycompany/databaseproject2/GTRatingApplication.java
+++ src/main/java/com/mycompany/databaseproject2/GTRatingApplication.java
@@ -1,5 +1,6 @@
 package com.mycompany.databaseproject2;
 
+import com.mycompany.databaseproject2.domains.*;
 import com.mycompany.databaseproject2.repositories.*;
 import com.datastax.driver.core.Session;
 import java.util.Scanner;
@@ -21,6 +22,7 @@ public class GTRatingApplication {
         
         // create the "drinks" keyspace
         KeyspaceRepository StdRep = new KeyspaceRepository(session);
+        
         // create the various domain repositories
         GinRepository ginRep = new GinRepository(session);
         TonicRepository tonicRep = new TonicRepository(session);
@@ -36,6 +38,51 @@ public class GTRatingApplication {
         // branch out, depending on the input given
         while (line != "quit" || line != "stop" || line != "exit") {
             switch (line) {
+                
+                // create a new gin, tonic or garnish
+                case "insert":
+                    System.out.println("Ayyy mah boi, u tryna insert new stuff?\nIs it a gin, tonic or a maddafakkin garnish eh?");
+                    line = sc.nextLine();
+                    switch (line) {
+                        
+                        // insert a new gin
+                        case "gin":
+                            System.out.println("What is the name of the gin?");
+                            String ginName = sc.nextLine();
+                            System.out.println("What is the price of the gin?");
+                            String ginPrice = sc.nextLine();
+                            System.out.println("Inserting new gin...");
+                            ginRep.insertgin(new Gin(ginName, Float.valueOf(ginPrice), Float.valueOf(ginPercentage)));
+                            System.out.println("New gin succesfully inserted!");
+                            break;
+                            
+                        // insert a new tonic 
+                        case "tonic":
+                            System.out.println("What is the name of the tonic?");
+                            String tonicName = sc.nextLine();
+                            System.out.println("What is the price of the tonic?");
+                            String tonicPrice = sc.nextLine();
+                            System.out.println("Inserting new tonic...");
+                            tonicRep.insertTonic(new Tonic(tonicName, Float.valueOf(tonicPrice)));
+                            System.out.println("New tonic succesfully inserted!");
+                            break;
+                            
+                        // insert a new garnish
+                        case "garnish":
+                            System.out.println("What is the name of the garnish?");
+                            String garnishName = sc.nextLine();
+                            System.out.println("What is the price of the garnish?");
+                            String garnishPrice = sc.nextLine();
+                            System.out.println("Inserting new garnish...");
+                            garnishRep.insertGarnish(new Garnish(garnishName));
+                            System.out.println("New garnish succesfully inserted!");
+                            break;
+                           
+                        default:
+                            System.out.println("Unknown input: \"" + line + "\".");
+                            break;
+                    }
+                    break;
                 case "ayyy, cook some meth dawg":
                     //meth.cookHomie(bestShit);
                     break;
