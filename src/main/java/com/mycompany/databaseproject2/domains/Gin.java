dcaf959532a6ab73d73003d8301d0cdca05d6592
sebastian <sebastian@sebastian-Aspire-E5-774G>
2018-05-18 23:37:56 +0200

    gin

--- src/main/java/com/mycompany/databaseproject2/domains/Gin.java
+++ src/main/java/com/mycompany/databaseproject2/domains/Gin.java
@@ -11,6 +11,13 @@ package com.mycompany.databaseproject2.domains;
  */
 public class Gin {
     
-    String name;
+    public String name;
+    public float price;
+    public float percentage;
     
+    public Gin(String name, float price, float percentage){
+        this.name=name;
+        this.price=price;
+        this.percentage=percentage;
+    }
 }
