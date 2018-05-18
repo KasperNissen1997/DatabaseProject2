64965006c0108d3093b17d43278c0a6d5b06ea29
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 18:52:59 +0200

    Implemented the CassandraConnector

--- src/main/java/com/mycompany/databaseproject2/CassandraConnector.java
+++ src/main/java/com/mycompany/databaseproject2/CassandraConnector.java
@@ -5,10 +5,42 @@
  */
 package com.mycompany.databaseproject2;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Builder;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+
 /**
+ * 
+ * This is an implementation of a simple Java client.
  *
- * @author Kaspe
  */
 public class CassandraConnector {
-    
+
+    private Cluster cluster;
+
+    private Session session;
+
+    public void connect(final String node, final Integer port) {
+
+        Builder b = Cluster.builder().addContactPoint(node);
+
+        if (port != null) {
+            b.withPort(port);
+        }
+        cluster = b.build();
+
+        Metadata metadata = cluster.getMetadata();
+
+        session = cluster.connect();
+    }
+
+    public Session getSession() {
+        return this.session;
+    }
+
+    public void close() {
+        session.close();
+        cluster.close();
+    }
 }
