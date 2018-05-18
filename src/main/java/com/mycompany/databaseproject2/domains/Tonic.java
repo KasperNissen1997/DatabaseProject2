88666a26febddfae6aa92dd254cf15432c1a182f
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 19:56:11 +0200

    Implemented get-, setters and a constructor

--- src/main/java/com/mycompany/databaseproject2/domains/Tonic.java
+++ src/main/java/com/mycompany/databaseproject2/domains/Tonic.java
@@ -1,16 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.databaseproject2.domains;
 
-/**
- *
- * @author Kaspe
- */
 public class Tonic {
     
-    String name;
+    private String name;
+    private float price;
     
+    public Tonic(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    public float getPrice() {
+        return this.price;
+    }
 }
