50376a6f8fdef2b667eeb2bee1c9db1649a534c2
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-19 00:35:37 +0200

    Started implementation of the garnish

--- src/main/java/com/mycompany/databaseproject2/domains/Garnish.java
+++ src/main/java/com/mycompany/databaseproject2/domains/Garnish.java
@@ -1,16 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.databaseproject2.domains;
 
 /**
  *
- * @author Kaspe
+ * @author KasperNissen1997
  */
 public class Garnish {
     
-    String name;
+    private String name;
+    
+    public Garnish(String name) {
+        this.name = name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
     
 }
--- src/main/java/com/mycompany/databaseproject2/repositories/GarnishRepository.java
+++ src/main/java/com/mycompany/databaseproject2/repositories/GarnishRepository.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.databaseproject2.repositories;
+
+import com.mycompany.databaseproject2.domains.Garnish;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
 
 /**
  *
- * @author Kaspe
+ * @author KasperNissen1997
  */
 public class GarnishRepository {
+    private Session session;
+    
+    public static final String TABLE_NAME = "garnish";
+    
+    public GarnishRepository(Session session) {
+        this.session = session;
+    }
+    
+    /**
+     * Creates the garnish table.
+     */
+    public void createTable() {
+        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
+                .append(TABLE_NAME)
+                .append("(")
+                .append("name text PRIMARY KEY")
+                .append(");");
+
+        final String query = sb.toString();
+        session.execute(query);
+    }
+    
     
 }
