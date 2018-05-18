f63693bda238578cc38ef8ff5a7398e8598033ca
sebastian <sebastian@sebastian-Aspire-E5-774G>
2018-05-19 00:34:13 +0200

    create rating tables

--- src/main/java/com/mycompany/databaseproject2/repositories/RatingRepository.java
+++ src/main/java/com/mycompany/databaseproject2/repositories/RatingRepository.java
@@ -5,10 +5,34 @@
  */
 package com.mycompany.databaseproject2.repositories;
 
+import com.datastax.driver.core.Session;
+
 /**
  *
  * @author Kaspe
  */
 public class RatingRepository {
+    public static final String RATING_BY_USER = "rtbusr";
+    public static final String RATING_BY_COMB = "rtbusr";
+    
+    
+    Session session;
+    
+    public void RatingRepository(Session session){
+        this.session=session;
+    }
+    
+    public void createTable() {
+        StringBuilder f = new StringBuilder("BEGIN BATCH ");
+        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXIST ").append(RATING_BY_USER).append("(").append("user UUID PRIMARY KEY, ").append("rating int, ").append("eval text, ").append("comb tuple, ").append("marks int").append(");");
+        f.append(sb.toString());
+        
+        sb = new StringBuilder("CREATE TABLE IF NOT EXIST ").append(RATING_BY_COMB).append("(").append("user UUID, ").append("rating int, ").append("eval text, ").append("comb tuple PRIMARY KEY, ").append("marks int").append(");");
+        f.append(sb.toString());
+        f.append("END BATCH;");
+        final String query = f.toString();
+        session.execute(query);
+    }
+    
     
 }
