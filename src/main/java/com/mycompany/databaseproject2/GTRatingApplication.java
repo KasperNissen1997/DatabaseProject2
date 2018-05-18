2eebec3d85b1d0dd1507ddf02cc7454cddb9df11
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 18:44:13 +0200

    Added various files

---
+++ src/main/java/com/mycompany/databaseproject2/GTRatingApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.databaseproject2;
+
+/**
+ *
+ * @author Kaspe
+ */
+public class GTRatingApplication {
+    
+    static void main(String[] args) {
+        GTRatingApplication.runApplication();
+    }
+    
+    static void runApplication() {
+        
+    }
+}
