9aeb1e938d415f52795e0a9b5606f1e8f75772bf
KasperNissen1997 <KasperNissen1997@gmail.com>
2018-05-18 19:34:40 +0200

    Started the implementation of the tonics table

--- src/main/java/com/mycompany/databaseproject2/repositories/TonicRepository.java
+++ src/main/java/com/mycompany/databaseproject2/repositories/TonicRepository.java
@@ -2,14 +2,27 @@ package com.mycompany.databaseproject2.repositories;
 
 import com.datastax.driver.core.Session;
 
-/**
- *
- * @author Kaspe
- */
 public class TonicRepository {
     private Session session;
+    
+    public static final String TABLE_NAME = "tonics";
     
     public TonicRepository(Session session) {
         this.session = session;
     }
+    
+    /**
+     * Creates the tonics table.
+     */
+    public void createTable() {
+        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
+                .append(TABLE_NAME)
+                .append("(")
+                .append("name text PRIMARY KEY);");
+
+        final String query = sb.toString();
+        session.execute(query);
+    }
+    
+    
 }
