aa37b80d153edbbda9ce4ac6bc40eb2bd166625e
sebastian <sebastian@sebastian-Aspire-E5-774G>
2018-05-19 17:45:09 +0200

    pompom xml dependency

--- src/main/java/com/mycompany/databaseproject2/domains/Rating.java
+++ src/main/java/com/mycompany/databaseproject2/domains/Rating.java
@@ -13,5 +13,55 @@ public class Rating {
     String comment;
     int rating;
     int marks;
+
+    public Rating(UUID author, Combination comb, String comment, int rating, int marks) {
+        this.author = author;
+        this.comb = comb;
+        this.comment = comment;
+        this.rating = rating;
+        this.marks = marks;
+    }
+
+    
+    
+    public UUID getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UUID author) {
+        this.author = author;
+    }
+
+    public Combination getComb() {
+        return comb;
+    }
+
+    public void setComb(Combination comb) {
+        this.comb = comb;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
     
 }
--- src/main/java/com/mycompany/databaseproject2/repositories/RatingRepository.java
+++ src/main/java/com/mycompany/databaseproject2/repositories/RatingRepository.java
@@ -1,6 +1,11 @@
 package com.mycompany.databaseproject2.repositories;
 
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import com.mycompany.databaseproject2.domains.Combination;
+import com.mycompany.databaseproject2.domains.Rating;
+import com.mycompany.databaseproject2.domains.User;
+import java.util.List;
 
 /**
  *
@@ -8,7 +13,7 @@ import com.datastax.driver.core.Session;
  */
 public class RatingRepository {
     public static final String RATING_BY_USER = "rtbusr";
-    public static final String RATING_BY_COMB = "rtbusr";
+    public static final String RATING_BY_COMB = "rtbcom";
     
     
     Session session;
@@ -29,5 +34,40 @@ public class RatingRepository {
         session.execute(query);
     }
     
+    public ResultSet selectByComb(Combination comb){
+        StringBuilder sb = new StringBuilder("SELECT * FROM ").append(RATING_BY_COMB).append(" WHERE comb = ('").append(comb.getGinName()).append("', '").append(comb.getTonicName()).append("', '").append(comb.getGarnishName()).append("');");
+        final String query = sb.toString();
+        ResultSet rs = session.execute(query);
+        return rs;
+    }
+    
+    public ResultSet selectByUser(User user){
+        StringBuilder sb = new StringBuilder("SELECT * FROM ").append(RATING_BY_USER).append(" WHERE user = ").append(user.getId()).append(";");
+        final String query = sb.toString();
+        ResultSet rs = session.execute(query);
+        return rs;
+    }
+    
+    public void createRating(Rating rating){
+        StringBuilder q = new StringBuilder("BEGIN BATCH ");
+        
+        StringBuilder q1 = new StringBuilder("INSERT INTO ").append(RATING_BY_USER).append("(user, rating, eval,comb, marks) VALUES(")
+                .append(rating.getAuthor())
+                .append(", "+rating.getRating())
+                .append(", "+rating.getComment())
+                .append(", "+rating.getComb())
+                .append(", "+rating.getMarks())
+                .append(");");
+        
+        StringBuilder q2 = new StringBuilder("INSERT INTO ").append(RATING_BY_COMB).append("(user, rating, eval,comb, marks) VALUES(")
+                .append(rating.getAuthor())
+                .append(", "+rating.getRating())
+                .append(", "+rating.getComment())
+                .append(", "+rating.getComb())
+                .append(", "+rating.getMarks())
+                .append(");");
+        
+        StringBuilder q3 = new StringBuilder("UPDATE ").append();
+    }
     
 }
